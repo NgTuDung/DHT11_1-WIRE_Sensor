2f9df08c4725f4723412dc4ea9aac085fbd0c516
NgTuDung <nguyentuandungubqn@gmail.com>
2024-09-19 11:11:32 +0700

    Update new .gitignore file

--- GNU ARM v12.2.1 - Debug/gecko_sdk_4.4.2/platform/service/iostream/src/sl_iostream_stdlib_config.d
+++
@@ -1,4 +0,0 @@
-gecko_sdk_4.4.2/platform/service/iostream/src/sl_iostream_stdlib_config.o: \
- C:/Users/FPTSHOP/SimplicityStudio/SDKs/gecko_sdk/platform/service/iostream/src/sl_iostream_stdlib_config.c \
- c:\users\fptshop\simplicitystudio\sdks\gecko_sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h
-c:\users\fptshop\simplicitystudio\sdks\gecko_sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h:
--- GNU ARM v12.2.1 - Debug/gecko_sdk_4.4.2/platform/service/system/src/sl_system_init.d
+++
@@ -1,6 +0,0 @@
-gecko_sdk_4.4.2/platform/service/system/src/sl_system_init.o: \
- C:/Users/FPTSHOP/SimplicityStudio/SDKs/gecko_sdk/platform/service/system/src/sl_system_init.c \
- c:\users\fptshop\simplicitystudio\sdks\gecko_sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h \
- C:\Users\FPTSHOP\SimplicityStudio\v5_workspace\DHT11_Sensor\autogen/sl_event_handler.h
-c:\users\fptshop\simplicitystudio\sdks\gecko_sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h:
-C:\Users\FPTSHOP\SimplicityStudio\v5_workspace\DHT11_Sensor\autogen/sl_event_handler.h:
